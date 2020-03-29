ab41222b5fe6a4bac2fd6849870fa5cba09b61d2
songjein <jeinsong100@gmail.com>
2020-03-29 15:36:43 +0000

    wrapping REST API

--- graphql/db.js
+++ graphql/db.js
@@ -73,3 +73,21 @@ export const addMovie = (name, score) => {
 	movies.push(newMovie);
 	return newMovie;
 }
+
+
+// Wrapping Movie API
+import fetch from "node-fetch"
+const API_URL= "https://yts.am/api/v2/list_movies.json?"
+
+export const getMoviesFromAPI = (limit, rating) => {
+	let REQUEST_URL = API_URL;
+	if (limit > 0) {
+		REQUEST_URL += `limit=${limit}`
+	}
+	if (rating > 0) {
+		REQUEST_URL += `&minimum_rating=${rating}`;
+	}
+	return fetch(`${REQUEST_URL}`)
+		.then(res => res.json())
+		.then(json => json.data.movies);
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies, getById, addMovie, deleteMovie, getMoviesFromAPI } from "./db";
 
 const resolvers = {
 	Query: {
 		movies: () => getMovies(),
-		movie: (_, { id }) => getById(id)
+		movie: (_, { id }) => getById(id),
+		movies_from_api: (_, { limit, rating }) => getMoviesFromAPI(limit, rating)
 	},
 	Mutation: {
-		addMovie: (_, {name, score}) => addMovie(name, score),
-		deleteMovie: (_, {id}) => deleteMovie(id)
+		addMovie: (_, { name, score }) => addMovie(name, score),
+		deleteMovie: (_, { id }) => deleteMovie(id)
 	},
 }
 
