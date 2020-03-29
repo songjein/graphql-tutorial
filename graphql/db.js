993906a27e6b60c0d7f752e4482b3c3c3d09fbef
songjein <jeinsong100@gmail.com>
2020-03-29 15:03:05 +0000

    mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,75 @@
-export const people = [{
+// export const people = [{
+// 		id: 1,
+// 		name: "jeinsong",
+// 		age: 22,
+// 		gender: "male",
+// 	},
+// 	{
+// 		id: 2,
+// 		name: "songjein",
+// 		age: 24,
+// 		gender: "male",
+// 	},
+// 	{
+// 		id: 3,
+// 		name: "jesongin",
+// 		age: 23,
+// 		gender: "female",
+// 	}
+// ];
+
+
+// export const getById = id => {
+// 	const filteredPeople = people.filter(person => person.id === id);
+// 	return filteredPeople[0];
+// }
+
+let movies = [
+	{
+		id: 0,
+		name: "Star Wars - The new one",
+		score: 1
+	},
+	{
 		id: 1,
-		name: "jeinsong",
-		age: 22,
-		gender: "male",
+		name: "Avengers - The new one",
+		score: 8
 	},
 	{
 		id: 2,
-		name: "songjein",
-		age: 24,
-		gender: "male",
+		name: "The Godfather I",
+		score: 99,
 	},
 	{
 		id: 3,
-		name: "jesongin",
-		age: 23,
-		gender: "female",
+		name: "Logan",
+		score: 2,
 	}
 ];
 
+export const getMovies = () => movies;
 
 export const getById = id => {
-	const filteredPeople = people.filter(person => person.id === id);
-	return filteredPeople[0];
+	const filteredMovies = movies.filter(movie => movie.id === id);
+	return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+	const cleanedMovies = movies.filter(movie => movie.id !== id);
+	if (movies.length > cleanedMovies.length) {
+		movies = cleanedMovies;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+export const addMovie = (name, score) => {
+	const newMovie = {
+		id: movies.length + 1,
+		name,
+		score,
+	}
+	movies.push(newMovie);
+	return newMovie;
 }
